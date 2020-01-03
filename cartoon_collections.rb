2a71071c35811811b97012608b7ddeac2a500821
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 11:43:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#code an argument here
+  def roll_call_dwarves(array)
+    array.each_with_index do |name,index|
+      puts "#{index+1}. #{name}"
+    end
+  end
+
+# end # Your code here
+
+def summon_captain_planet (array)
+  array.collect do|word|
+  word.capitalize + "!"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  array.any? do|word|
+  word.length > 4
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
+  
+  #code an argument here
+  # the array below is here to help
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+
